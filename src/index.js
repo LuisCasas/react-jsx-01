8b50ac5e2a25e357eaa7c36813360d2ca7376a7a
LuisCasas <sc.luis@gmail.com>
2019-02-13 23:11:05 +0000

    Adding basic comment component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Comment from './components/Comment';
 
 const App = () => {
-    return <div>Hey! React</div>
+
+    return (
+        <div className="ui container comments">
+            <Comment />
+            <Comment />
+            <Comment />
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
